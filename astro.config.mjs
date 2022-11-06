841ccf01db32148dc2e6d620176035f17eb3adf2
MarvelSQ <sun1993hlj@hotmail.com>
2022-11-06 21:50:14 +0800

    build: config vercel static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), svelte()],
+  adapter: vercel(),
 });
